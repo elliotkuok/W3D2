3d784321509845d00365fd01a74bc27a2bc92a08
Elliot Kuok <elliotckuok@gmail.com>
2023-06-06 16:27:39 -0700

    finished populate method

--- memory_game/board.rb
+++ memory_game/board.rb
@@ -1,28 +1,32 @@
 require "./card.rb"
 class Board
     def initialize(n)
-        @grid = Array.new(n) {Array.new(n)}
-    end
+      @grid = Array.new(n) { Array.new(n) }
+      @size = n * n
+      self.populate
 
+    end
+  
     def card_maker
-        card_arr = []
-        card_values = ('a'..'z').to_a
-        card_values.each do |value|
-            card_arr << Card.new(value) 
-            card_arr << Card.new(value) 
+      card_arr = []
+      card_values = ('a'..'z').to_a
+      card_values.each do |value|
+        if card_arr.length < @size
+            card_arr << Card.new(value)
+            card_arr << Card.new(value)
         end
-        card_arr
+      end
+      card_arr
     end
-
+  
     def populate
-        cards = card_maker
-        cards.shuffle!
-        @grid.each_with_index do |row, i|
-            row.each_with_index do |ele, j|
-                @grid[i][j] = cards.pop
-            end
+      cards = card_maker
+      cards.shuffle!
+
+      @grid.each_with_index do |row, i|
+        row.each_with_index do |ele, j|
+          @grid[i][j] = cards.pop
         end
+      end
     end
-
-
 end
